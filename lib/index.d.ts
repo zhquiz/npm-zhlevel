f7800cd4b19591e45b0070e832786b9c85fd4847
Pacharapol Withayasakpunt <polv@polv.cc>
2021-10-10 15:03:49 +0700

    move from zhquiz/packages/zhlevel

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+import sqlite3 from 'better-sqlite3';
+export declare class Level {
+    db: sqlite3.Database;
+    V_LEVEL_POW: number;
+    hLevel(v: string): number;
+    vLevel(v: string): any;
+}
+export declare function makePinyin(entry: string): any;
+//# sourceMappingURL=index.d.ts.map
